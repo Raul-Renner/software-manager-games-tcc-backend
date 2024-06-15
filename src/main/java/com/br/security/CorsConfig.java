d9ccccecde2a4debc97a49dc3fefa92239bd7954
Raul-Renner <raul.renner.9824@outlook.com>
2024-06-15 20:49:05 -0300

    update url postgres and update package in corsFilter

--- src/main/java/com/br/security/CorsConfig.java
+++ src/main/java/com/br/security/CorsConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig{
     @Bean
-    public org.springframework.web.filter.CorsFilter corsFilter() {
+    public CorsFilter corsFilter() {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
